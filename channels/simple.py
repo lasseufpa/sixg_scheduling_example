bfe44c82fe83bf07f5cdcf81ac9d29032abe2752
Cleverson Nahum <cleversonahum@gmail.com>
2023-03-07 11:42:55 -0300

    Adapting channel step to receive sched_decision

--- channels/simple.py
+++ channels/simple.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 
 from sixg_radio_mgmt import Channel
@@ -16,7 +18,11 @@ class SimpleChannel(Channel):
         )
 
     def step(
-        self, step_number: int, episode_number: int, mobilities: np.ndarray
+        self,
+        step_number: int,
+        episode_number: int,
+        mobilities: np.ndarray,
+        sched_decision: Optional[np.ndarray] = None,
     ) -> np.ndarray:
         spectral_efficiencies = [
             np.ones((self.max_number_ues, self.num_available_rbs[i]))
